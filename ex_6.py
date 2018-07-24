de3b018dedf7ad7aa104839881b35e2426b16749
neelima513 <neelimakottapati@gmail.com>
2018-07-24 13:38:49 +0530

    seventh commit eighth commit 9th commit 10th commit

---
+++ ex_6.py
@@ -0,0 +1,12 @@
+default_order="{},{}and{}".format('john','bill','sean')
+print('\n---default order---')
+print(default_order)
+
+
+positional_order="{1}, {0}and {2}".format('john','bill','sean')
+print('\n--positional_order--')
+print(positional_order)
+
+keyword_order="{s},{b}and{j}".format(j='john',b='bill',s='sean')
+print('\n----keyword_order----')
+print(keyword_order)
---
+++ ex_7.py
@@ -0,0 +1,9 @@
+days="mon tue wed thu fri sat sun"
+months="jan\nfeb\nmar\napr\nmay\njun\njuly\naug\nsep\noct\nnov\ndec"
+print("here are the days:",days)
+print("here are the months:",months)
+print("""there's somthing going on here.
+	with the three double_quotes.
+	we'll be able to type as much as we like.
+	even 4 lines if we want ,or 5, or 6.
+	""") 
