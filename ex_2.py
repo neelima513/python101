e9f46cd4bbd0e3cc8187bda57428c7dda8f5abad
neelima513 <41414437+neelima513@users.noreply.github.com>
2018-07-25 16:09:39 +0530

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -5,3 +5,12 @@ print("This is fun.")
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+sample output:
+  Hello World!
+Hello Again
+I like typing this.
+This is fun.
+Yay! Printing.
+I'd much rather you 'not'.
+I "said" do not touch this.
