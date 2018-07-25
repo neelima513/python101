227fb32c7ae7e396bd1902e4d1bf94f4373f12db
neelima513 <41414437+neelima513@users.noreply.github.com>
2018-07-25 16:06:56 +0530

    Update ex_9.py

--- ex_9.py
+++ ex_9.py
@@ -13,3 +13,16 @@ print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+sample output:
+  
+   I'm tabbed in.
+I'm split
+on a line.
+I'm \ a \ cat.
+
+I'll do a list:
+        * Cat food
+        * Fishies
+        * Catnip
+        * Grass
