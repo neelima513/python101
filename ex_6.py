3c9d211c5cf92adafa482e90e4469df63e3c242f
neelima513 <41414437+neelima513@users.noreply.github.com>
2018-07-25 16:51:37 +0530

    Update ex_6.py

--- ex_6.py
+++ ex_6.py
@@ -10,3 +10,18 @@ print(positional_order)
 keyword_order="{s},{b}and{j}".format(j='john',b='bill',s='sean')
 print('\n----keyword_order----')
 print(keyword_order)
+
+sample output:
+  
+  ---default order---
+john,billandsean
+
+--positional_order--
+bill, johnand sean
+
+----keyword_order----
+sean,billandjohn
+
+
+  
+  
