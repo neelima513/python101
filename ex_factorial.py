4000a56ad97b71d2dc30371762e5d21c71cc64c7
neelima513 <neelimakottapati@gmail.com>
2018-08-12 19:19:53 +0530

    factorial

--- ex_factorial.py
+++ ex_factorial.py
@@ -1,23 +1,24 @@
-def fact(n)=4
-fact=1
-for i in range(1,n+1):
-	fact=fact*i
-	print("the factorial of",n,"is",fact)
+#factorial
+'''the factorial number of 7 is: 5040'''
 
 def factorial(n):
-    factorial = 1
-    if n<0:
-       print("factorial number doesnot exits")
-    elif n==0:
-       print("the factorial of 0 is 1")
-    else:
-       for i in range(factorial,n+1):
-           factorial = factorial * i
-    print("the factorial number of",n,"is:",factorial)
+	factorial = 1
+	if n<0:
+		print("factorial number doesnot exits")
+	elif n==0:
+		print("the factorial of 0 is 1")
+	else:
+		for i in range(factorial,n+1):
+			factorial = factorial * i
+		print("the factorial number of",n,"is:",factorial)
 n = 7
 factorial(n)
 
-
+#bmi calculator
+'''input:enter the weight 39
+	enter the height 5.2
+output: 
+	1.442307692307692
 weight=float(input("enter the weight"))
 height=float(input("enter the height"))
-print(weight/(height*height)
+print(weight/(height*height))
