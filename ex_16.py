0b30baa015f797a19ee3de9331fc9dd60804ff00
neelima513 <neelimakottapati@gmail.com>
2018-08-12 19:30:51 +0530

    adding list values

--- ex_16.py
+++ ex_16.py
@@ -1,6 +1,7 @@
 numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
 sum = 0
-for index,item in numbers:
+for val in numbers:
 	sum = sum+val
-print("the index",index)
 print("The sum is", sum)
+'''output:
+	the sum is 48'''
