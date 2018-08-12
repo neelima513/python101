786c8e27674e841e6021587299d56c68b43a61eb
neelima513 <neelimakottapati@gmail.com>
2018-08-12 19:39:26 +0530

    intersection example

--- ex_intersection.py
+++ ex_intersection.py
@@ -17,3 +17,6 @@ lst1 = [4, 9, 1, 17, 11, 26, 28, 54, 69]
 lst2 = [9, 9, 74, 21, 45, 11, 63, 28, 26]
 print(intersection(lst1, lst2))
 print(intersection_set(lst1, lst2))
+''' output:
+	[9, 11, 26, 28]
+	[9, 26, 11, 28]'''
