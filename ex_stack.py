eaaee850bb23291da41a5c5b5747325b3c03a574
neelima513 <neelimakottapati@gmail.com>
2018-08-12 20:48:13 +0530

    stack

---
+++ ex_stack.py
@@ -0,0 +1,14 @@
+stack = ["Amar", "Akbar", "Anthony"]
+stack.append("Ram")
+stack.append("Iqbal")
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack)
+'''output:
+['Amar', 'Akbar', 'Anthony', 'Ram', 'Iqbal']
+Iqbal
+['Amar', 'Akbar', 'Anthony', 'Ram']
+Ram
+['Amar', 'Akbar', 'Anthony']'''
