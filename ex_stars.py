a28cf844770563e2215f55db7c576043544358e2
neelima513 <neelimakottapati@gmail.com>
2018-08-12 19:46:54 +0530

    printing stars in triangle pattern

--- ex_stars.py
+++ ex_stars.py
@@ -1,7 +1,18 @@
-num=int(input("ennter num of rows"))
+num=int(input("enter num of rows"))
 for i in range(0,num):
 	for j in range(0,i+1):
             print(" *",end="")
 	print("\n")
 	
 
+'''input:
+
+ennter num of rows 4
+output:
+ *
+
+ * *
+
+ * * *
+
+ * * * *
