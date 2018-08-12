96e33338fcf59bc862a144ec0dc345dd1d8c3a03
neelima513 <neelimakottapati@gmail.com>
2018-08-12 19:22:56 +0530

    sets

--- ex_insec.py
+++ ex_insec.py
@@ -1,3 +1,5 @@
+
+
 set1 = {2, 4, 5, 6} 
 set2 = {4, 6, 7, 8} 
 set3 = {4,6,8}
@@ -7,3 +9,6 @@ print("set1 intersection set2 : ", set1.intersection(set2))
  
 # union of three sets
 print("set1 intersection set2 intersection set3 :", set1.intersection(set2,set3))
+'''output:
+set1 intersection set2 :  {4, 6}
+set1 intersection set2 intersection set3 : {4, 6}'''
