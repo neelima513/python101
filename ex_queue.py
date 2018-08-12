43122ecbe251a1ea663e255ee388994b62f2bfb3
neelima513 <neelimakottapati@gmail.com>
2018-08-12 21:00:03 +0530

    queue

---
+++ ex_queue.py
@@ -0,0 +1,15 @@
+queue =["Ram", "Tarun"]
+print(queue)
+queue.append("Akbar")
+print(queue)
+queue.append("Birbal")
+print(queue) 
+print(queue.pop())
+print(queue)
+'''output:
+['Ram', 'Tarun']
+['Ram', 'Tarun', 'Akbar']
+['Ram', 'Tarun', 'Akbar', 'Birbal']
+Birbal
+['Ram', 'Tarun', 'Akbar']
+'''
