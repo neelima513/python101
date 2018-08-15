fe6b5286f5b36b3c4babc5d63f3a3103bba3dc7e
neelima513 <neelimakottapati@gmail.com>
2018-08-15 21:30:35 +0530

    stacks,queues

--- Milestone_Project_2.py
+++ Milestone_Project_2.py
@@ -4,6 +4,7 @@ class Stack:
 	def push(self,item):
 		self.item.append(item)
 		print(self.item)
+		
 	def pop(self):
 		try:
 		 	return self.item.pop()
@@ -20,13 +21,26 @@ stack.push('12')
 stack.push('vicky')
 stack.push(8)
 stack.push(9)
+stack.peek()
+print(stack.peek())
 stack.pop()
-
-
-
-
-
-
+stack.peek()
+print(stack.peek())
+stack.push(2)
+stack.peek()
+print(stack.peek())
+stack.pop()
+stack.push(5)
+stack.peek()
+print(stack.peek())
+stack.pop()
+print(stack.pop())
+stack.pop()
+print(stack.pop())
+stack.pop()
+print(stack.pop())
+stack.pop()
+print(stack.pop())
 '''
 output:
 ['nani']
@@ -35,16 +49,18 @@ output:
 ['nani', '12', 'vicky', 8]
 ['nani', '12', 'vicky', 8, 9]
 9
-9
 8
+['nani', '12', 'vicky', 8, 2]
+2
+['nani', '12', 'vicky', 8, 5]
+5
 8
-8
-8
-8
-vicky
 12
-nani
 there is an error
 None
+there is an error
+there is an error
+None
+
 
 '''
---
+++ ex_queue1.py
@@ -0,0 +1,6 @@
+queue=[10,5]
+queue.append('a')
+queue.append('b')
+queue.append('c')
+print(queue)
+print(queue.pop())
